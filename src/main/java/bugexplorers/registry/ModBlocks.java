b2939acb9981d13f3422dda7b8f8eb8b8db64fd3
Expora9 <lennyj2468+github@gmail.com>
2020-10-04 16:50:36 +0100

    Added Nether Hive Block

--- src/main/java/bugexplorers/registry/ModBlocks.java
+++ src/main/java/bugexplorers/registry/ModBlocks.java
@@ -2,8 +2,9 @@ package bugexplorers.registry;
 
 public class ModBlocks
 {
-    public static void RegisterBlocks(){
-
-
+  public static final Block NETHER_HIVE_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).hardness(4.0f));
+    public static void RegisterBlocks()
+    {
+     Registry.register(Registry.BLOCK, new Identifier(Beelectricity.ID, "nether_hive"), NETHER_HIVE_BLOCK);
     }
 }
