471706cd544a2476452a9c70b7774bd61ff135c4
Unknown Beetle <64570529+uknownbeetle@users.noreply.github.com>
2020-10-06 22:29:35 +1100

    Update BeelectricityBlocks.java

--- src/main/java/bugexplorers/beelectricity/registries/BeelectricityBlocks.java
+++ src/main/java/bugexplorers/beelectricity/registries/BeelectricityBlocks.java
@@ -3,10 +3,10 @@ package bugexplorers.beelectricity.registries;
 import bugexplorers.beelectricity.Beelectricity;
 import bugexplorers.beelectricity.blocks.CrystalHoneyBlock;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.*;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class BeelectricityBlocks {
