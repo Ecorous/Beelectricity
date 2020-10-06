0675c120b8d03b94d57332bc51e512bb5632d2ba
Expora9 <49562894+Expora9@users.noreply.github.com>
2020-10-06 11:29:42 +0100

    Temp Removed This

--- src/main/java/bugexplorers/beelectricity/entities/WorkerBeeEntity.java
+++ src/main/java/bugexplorers/beelectricity/entities/WorkerBeeEntity.java
@@ -4,14 +4,14 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.BeeEntity;
 import net.minecraft.world.World;
 
-public class WorkerBeeEntity extends BeeEntity {
+//public class WorkerBeeEntity extends BeeEntity {
 
-    public WorkerBeeEntity(EntityType<? extends WorkerBeeEntity> entityType, World world) {
-        super(entityType, world);
-    }
+  //  public WorkerBeeEntity(EntityType<? extends WorkerBeeEntity> entityType, World world) {
+   //     super(entityType, world);
+ //   }
 
-    @Override
-    protected void initGoals() {
+    //@Override
+    //protected void initGoals() {
 
-    }
-}
+  //  }
+//}
