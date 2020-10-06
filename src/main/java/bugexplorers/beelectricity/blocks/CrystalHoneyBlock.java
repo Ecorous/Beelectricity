04e7542e81fff42c914c68cbd328b7cfa996865b
Expora9 <49562894+Expora9@users.noreply.github.com>
2020-10-06 12:33:22 +0100

    Update CrystalHoneyBlock.java

--- src/main/java/bugexplorers/beelectricity/blocks/CrystalHoneyBlock.java
+++ src/main/java/bugexplorers/beelectricity/blocks/CrystalHoneyBlock.java
@@ -2,12 +2,27 @@ package bugexplorers.beelectricity.blocks;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.WorldView;
 
 
 public class CrystalHoneyBlock extends ScaffoldingBlock
 {
+    @Override
+    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        return true;
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
+        return VoxelShapes.cuboid(0f, 0f, 0f, 1f, 1.0f, 1.0f);
+    }
 
     public CrystalHoneyBlock(FabricBlockSettings hardness) {
         super(AbstractBlock.Settings.of(Material.STONE).nonOpaque());
+
     }
 }
